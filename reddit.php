2d79bb3b9f0cc4d92015a2bfb76e5d810b720e51
Andrusowski <web@andrus.io>
2018-09-15 18:28:13 +0200

    improved posttitle cleanup

--- reddit.php
+++ reddit.php
@@ -3,9 +3,43 @@ class Reddit {
 
     private static $lastParse;
 
+    //consts taken from christopher-dG's osu-bot
+    private static $title_ignores = [
+        'UNNOTICED',
+        'UNNOTICED?',
+        'RIPPLE',
+        'GATARI',
+        'UNSUBMITTED',
+        'OFFLINE',
+        'RESTRICTED',
+        'BANNED',
+        'UNRANKED',
+        'LOVED',
+        'STANDARD',
+        'STD',
+        'OSU!',
+        'O!STD',
+        'OSU!STD',
+        'OSU!STANDARD',
+        'TAIKO',
+        'OSU!TAIKO',
+        'O!TAIKO',
+        'CTB',
+        'O!CATCH',
+        'OSU!CATCH',
+        'CATCH',
+        'OSU!CTB',
+        'O!CTB',
+        'MANIA',
+        'O!MANIA',
+        'OSU!MANIA',
+        'OSU!M',
+        'O!M',
+    ];
+
     public function new() {
         //$content=file_get_contents("https://ga.reddit.com/r/osugame/new.json?limit=100");
-        $content=file_get_contents("https://ga.reddit.com/r/osugame/search.json?q=flair%3AGameplay&sort=new&restrict_sr=on&t=all");
+        $content=file_get_contents('https://ga.reddit.com/r/osugame/search.json?q=flair%3AGameplay&sort=new&restrict_sr=on&t=all');
         $posts = json_decode($content);
 
         //go through all new posts and parse, if not in the Database
@@ -36,7 +70,7 @@ class Reddit {
             //update non-final post, if it already exists in the database
             else {
                 $row = $result->fetch_assoc();
-                if ($row["final"] == 0) {
+                if ($row['final'] == 0) {
                     if ($age >= 24*60*60) {
                         Database::updatePost($post, 1);
                     }
@@ -56,7 +90,7 @@ class Reddit {
         self::$lastParse = time();
 
         while ($after < time() - 60*60) { //stop archiving, when posts are younger than an hour
-            $content = file_get_contents("https://api.pushshift.io/reddit/submission/search?subreddit=osugame&sort=asc&limit=100&after=".$after);
+            $content = file_get_contents('https://api.pushshift.io/reddit/submission/search?subreddit=osugame&sort=asc&limit=100&after='.$after);
             $posts = json_decode($content);
             echo date('d/m/Y', $after)."\n";
 
@@ -93,37 +127,39 @@ class Reddit {
         /* check for characteristic characters from the already established format
         Player Name | Song Artist - Song Title [Diff Name] +Mods */
         $postTitle = $post->title;
-        if (strpos($postTitle, "|") &&
-        strpos($postTitle, "-") &&
-        strpos($postTitle, "["))
+        if (strpos($postTitle, '|') &&
+        strpos($postTitle, '-') &&
+        strpos($postTitle, '['))
         {
-            //parse relevant information from post title
+            //clean up posttitle from various annotations
+            $postTitle = preg_replace('/([\[\(]\#.*[\]\)])/U', '', $postTitle);
+
+            foreach(self::$title_ignores as $ignore) {
+                $postTitle = preg_replace("/([\[\(]\Q".$ignore."\E[\]\)])/i", '', $postTitle);
+            }
+
+            ////parse relevant information from post title
             $parsedPost = array(
-                "player" => "**error**",
-                "artist" => "**error**",
-                "title" => "**error**",
-                "diff" => "**error**",
+                'player' => '**error**',
+                'artist' => '**error**',
+                'title' => '**error**',
+                'diff' => '**error**',
             );
 
             $parseError = false;
             $matches = array();
+
             //Player
-            $match = preg_match("/(.+)\s+[\|丨].+-.+?\[.+?\]/", $postTitle, $matches);
+            $match = preg_match("/(.+)\s*[\|丨].+-.+?\[.+?\]/", $postTitle, $matches);
             if ($match != FALSE && count($matches) == 2) {
-                $parsedPost["player"] = $matches[1];
+                $parsedPost['player'] = trim($matches[1]);
             }
             else {
-                $match = preg_match("/(.+)\s*[\|丨].+-.+?\[.+?\]/", $postTitle, $matches);
-                if ($match != FALSE && count($matches) == 2) {
-                    $parsedPost["player"] = $matches[1];
-                }
-                else {
-                    $parseError = true;
-                }
+                $parseError = true;
             }
 
             //map Data
-            $tmpMap = "";
+            $tmpMap = '';
             $match = preg_match("/.+[\|丨]\s+(.+-.+\[.+\])/", $postTitle, $matches);
             if ($match != FALSE && count($matches) == 2) {
                 $tmpMap = $matches[1];
@@ -141,9 +177,9 @@ class Reddit {
             //split map Data
             $match = preg_match("/(.+)\s-\s(.+?)\s\[(.+?)\]/", $tmpMap, $matches);
             if ($match != FALSE && count($matches) == 4) {
-                $parsedPost["artist"] = $matches[1];
-                $parsedPost["title"] = $matches[2];
-                $parsedPost["diff"] = $matches[3];
+                $parsedPost['artist'] = trim($matches[1]);
+                $parsedPost['title'] = trim($matches[2]);
+                $parsedPost['diff'] = trim($matches[3]);
             }
             else {
                 $parseError = true;
@@ -153,10 +189,10 @@ class Reddit {
             if ($parseError == false) {
                 echo("\nOriginal: ".$post->title."\n"); //Debug
                 //check some additional stuff before marking as final
-                echo("Parsed: ".$parsedPost["player"]." | ".
-                $parsedPost["artist"]. " - ".
-                $parsedPost["title"].
-                $parsedPost["diff"]."\n");
+                echo('Parsed: '.$parsedPost['player'].' | '.
+                $parsedPost['artist']. ' - '.
+                $parsedPost['title']. ' [' .
+                $parsedPost['diff']."]\n");
 
                 //take a break to prevent osu!api spam
                 while (self::$lastParse == time()) {
@@ -164,7 +200,7 @@ class Reddit {
                 }
                 self::$lastParse = time();
 
-                $content = file_get_contents("https://www.reddit.com/r/osugame/comments/".$post->id.".json");
+                $content = file_get_contents('https://www.reddit.com/r/osugame/comments/'.$post->id.'.json');
                 $post = json_decode($content)[0]->data->children[0]->data;
 
                 Database::preparePost($post, $parsedPost, $final);
